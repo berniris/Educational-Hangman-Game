90ead908aaaaa31cb6ad2c8480dde4997979e014
berniris <alanacoleman01@gmail.com>
2018-03-29 21:13:22 -0400

    Saving work

--- src/app.js
+++ src/app.js
@@ -1,1 +1,81 @@
- const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+$(document).ready(function () {
+  const load = $('.load');
+  const play = $('.play');
+  const landing = $('.landing');
+  let buttons;
+
+console.log(play);
+
+  const questions = {
+    question1: 'British actor Boris Karloff created a cinematic icon when he played the role of what monster?',
+    question2: 'In a horror movie, you should worry if you encounter a doll named what'
+  }
+
+  let answers = {
+    question1: ['Dracula', 'Werewolf', 'Alien', 'Frankenstein'],
+    question2: ['A. Smiley', 'B. Bonnie', 'C. Chucky','D. Dolly']
+  };
+
+const correctAnswers = {
+  question1: 'Frankenstein',
+  question2: 'Chucky'
+}
+console.log(correctAnswers);
+
+console.log(correctAnswers);
+play.hide();
+load.click(loadGame);
+
+function loadGame(event) {
+  play.show();
+  landing.hide();
+}
+
+
+function printQuestion() {
+
+  play.append(questions.question1);
+}
+
+printQuestion();
+
+
+function createButtons () {
+
+  for(let i = 0; i < answers.question1.length; i++) {
+    buttons = $('<button></button>').text(answers.question1[i]);
+    play.append(buttons);
+  }
+  return buttons;
+}
+
+createButtons();
+
+$(buttons).click(function() {
+  console.log('clicked');
+  console.log($(this));
+    if (buttons.text() === correctAnswers.question1) {
+    play.hide();
+    $('<div>You won!</div>');
+  }
+
+});
+
+console.log(correctAnswers.question1);
+
+
+  // function chooseAnswer(event) {
+  //   if (event.target !== correctAnswers.question1 || correctAnswers.quetion2) {
+  //     play.hide()
+  //   }
+  // }
+  //     wrong.show()
+  //     }
+  //       else {
+  //         $(correct).show();
+  //         nextQuestions();
+  // }
+  // }
+  // chooseAnswer();
+});
+
